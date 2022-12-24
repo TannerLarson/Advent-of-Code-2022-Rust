a5acdf120af38e8a7aeca1253aba02ef00d8c1a6
Tanner Larson <tannerl@qualtrics.com>
2022-12-24 02:50:37 -0700

    Added more stuff

---
+++ rock-paper-scissors/target/debug/deps/rock_paper_scissors-3771e670b86d76b1.d
@@ -0,0 +1,6 @@
+/Users/tannerl/code/personal/advent-of-code-2022/rock-paper-scissors/target/debug/deps/rock_paper_scissors-3771e670b86d76b1: src/main.rs src/../input.txt
+
+/Users/tannerl/code/personal/advent-of-code-2022/rock-paper-scissors/target/debug/deps/rock_paper_scissors-3771e670b86d76b1.d: src/main.rs src/../input.txt
+
+src/main.rs:
+src/../input.txt:
---
+++ rock-paper-scissors/target/debug/rock-paper-scissors.d
@@ -0,0 +1,1 @@
+/Users/tannerl/code/personal/advent-of-code-2022/rock-paper-scissors/target/debug/rock-paper-scissors: /Users/tannerl/code/personal/advent-of-code-2022/rock-paper-scissors/input.txt /Users/tannerl/code/personal/advent-of-code-2022/rock-paper-scissors/src/main.rs
